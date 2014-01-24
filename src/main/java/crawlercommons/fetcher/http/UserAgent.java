300d6ebdb7f3814825322c73e5c53c7b6a41cf85
kkrugler_lists@transpac.com <kkrugler_lists@transpac.com@2727402e-d5ba-11de-bb02-e302c5db49d5>
2014-01-24 21:16:38 +0000

    Roll in patch from Lewis for issue #23 (http://code.google.com/p/crawler-commons/issues/detail?id=23)

--- src/main/java/crawlercommons/fetcher/http/UserAgent.java
+++ src/main/java/crawlercommons/fetcher/http/UserAgent.java
@@ -19,9 +19,27 @@ package crawlercommons.fetcher.http;
 
 import java.io.Serializable;
 
+import crawlercommons.CrawlerCommons;
+
+/**
+ * User Agent enables us to describe characteristics of any Crawler Commons agent. 
+ * There are a number of constructor options to describe the following:
+ * <ol>
+ * <li><tt>_agentName</tt>: Primary agent name.</li>
+ * <li><tt>_emailAddress</tt>: The agent owners email address.</li>
+ * <li><tt>_webAddress</tt>: A web site/address representing the agent owner.</li>
+ * <li><tt>_browserVersion</tt>: Broswer version used for compatibility.</li>
+ * <li><tt>_crawlerVersion</tt>: Version of the user agents personal crawler. If this is not set, 
+ * it defaults to the crawler commons maven artifact version.</li>
+ * </ol>
+ *
+ */
 @SuppressWarnings("serial")
 public class UserAgent implements Serializable {
+	
+	static CrawlerCommons cc;
 	public static final String DEFAULT_BROWSER_VERSION = "Mozilla/5.0";
+	public static final String DEFAULT_CRAWLER_VERSION = cc.getVersion();
 	
 	private final String _agentName;
 	private final String _emailAddress;
@@ -29,14 +47,35 @@ public class UserAgent implements Serializable {
 	private final String _browserVersion;
 	private final String _crawlerVersion;
 	
+	/**
+	 * Set user agent characteristics
+	 * @param agentName
+	 * @param emailAddress
+	 * @param webAddress
+	 */
 	public UserAgent(String agentName, String emailAddress, String webAddress) {
 		this(agentName, emailAddress, webAddress, DEFAULT_BROWSER_VERSION);
 	}
 
+	/**
+	 * Set user agent characteristics
+	 * @param agentName
+	 * @param emailAddress
+	 * @param webAddress
+	 * @param browserVersion
+	 */
 	public UserAgent(String agentName, String emailAddress, String webAddress, String browserVersion) {
-		this(agentName, emailAddress, webAddress, browserVersion, null);
+		this(agentName, emailAddress, webAddress, browserVersion, DEFAULT_CRAWLER_VERSION);
 	}
 	
+	/**
+	 * Set user agent characteristics
+	 * @param agentName
+	 * @param emailAddress
+	 * @param webAddress
+	 * @param browserVersion
+	 * @param crawlerVersion
+	 */
 	public UserAgent(String agentName, String emailAddress, String webAddress,
 			String browserVersion, String crawlerVersion) {
 		_agentName = agentName;
@@ -46,10 +85,18 @@ public class UserAgent implements Serializable {
 		_crawlerVersion = (crawlerVersion == null ? "" : "/" + crawlerVersion);
 	}
 	
+	/**
+	 * Obtain the just the user agent name
+	 * @return User Agent name (String)
+	 */
 	public String getAgentName() {
 		return _agentName;
 	}
 
+	/**
+	 * Obtain a String representing the user agent characteristics.
+	 * @return User Agent String
+	 */
 	public String getUserAgentString() {
 		// Mozilla/5.0 (compatible; mycrawler/1.0; +http://www.mydomain.com; mycrawler@mydomain.com)
 		return String.format("%s (compatible; %s%s; +%s; %s)",
