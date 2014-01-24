981308d4c6333c5ef641573552f76116c4642ad1
kkrugler_lists@transpac.com <kkrugler_lists@transpac.com@2727402e-d5ba-11de-bb02-e302c5db49d5>
2014-01-24 21:16:38 +0000

    Roll in patch from Lewis for issue #23 (http://code.google.com/p/crawler-commons/issues/detail?id=23)

---
+++ trunk/src/main/java/crawlercommons/CrawlerCommons.java
@@ -0,0 +1,25 @@
+package crawlercommons;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CrawlerCommons {
+
+    public String getVersion() {
+        String path = "/version.prop";
+        InputStream stream = CrawlerCommons.class.getResourceAsStream(path);
+        if (stream == null) {
+            return "Unknown Version";
+        }
+
+        Properties props = new Properties();
+        try {
+            props.load(stream);
+            stream.close();
+            return (String) props.get("version");
+        } catch (IOException e) {
+            return "Unknown Version";
+        }
+    }
+}
