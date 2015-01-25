19bc879d918d95f5752b3db6f2368e37b1c2ef09
avraham2@gmail.com <avraham2@gmail.com@2727402e-d5ba-11de-bb02-e302c5db49d5>
2015-01-25 09:18:01 +0000

    Issue 65:	[Sitemaps] Make SiteMapTool simpler by removing the Recursive flag
    Fixed the NPE issue
    Removed the recursive flag
    Upgraded javadocs

--- src/main/java/crawlercommons/sitemaps/SiteMapTester.java
+++ src/main/java/crawlercommons/sitemaps/SiteMapTester.java
@@ -6,50 +6,43 @@ import java.util.Collection;
 
 import org.apache.commons.io.IOUtils;
 
-/** Utility class for testing the Sitemap Parsing **/
+/** Sitemap Tool for recursively fetching all URL's from a sitemap (and all of it's children) **/
 public class SiteMapTester {
+    private static SiteMapParser parser = new SiteMapParser(false);
 
-    SiteMapParser parser = new SiteMapParser(false);
+    public static void main(String[] args) throws IOException, UnknownFormatException {
+        if (args.length < 1) {
+            System.err.println("Usage: SiteMapTester <URL_TO_TEST> [MIME_TYPE]");
+        } else {
+            URL url = new URL(args[0]);
+            String mt = (args.length > 1) ? args[1] : null;
 
-    private void parse(URL url, String mt, boolean recursive) throws IOException, UnknownFormatException {
+            parse(url, mt);
+        }
+    }
+
+    /** Parses a Sitemap recursively meaning that if the sitemap is a sitemapIndex then it parses all of the internal sitemaps */
+    private static void parse(URL url, String mt) throws IOException, UnknownFormatException {
         byte[] content = IOUtils.toByteArray(url);
 
         AbstractSiteMap sm = null;
-        
         // guesses the mimetype
         if (mt == null || mt.equals("")){
-        	sm = parser.parseSiteMap(content, url);
+        	  sm = parser.parseSiteMap(content, url);
+        } else {
+            sm = parser.parseSiteMap(mt, content, url);
         }
-        else 
-        	sm = parser.parseSiteMap(mt, content, url);
 
-        if (recursive && sm.isIndex()) {
+        if (sm.isIndex()) {
             Collection<AbstractSiteMap> links = ((SiteMapIndex) sm).getSitemaps();
             for (AbstractSiteMap asm : links) {
-                parse(asm.getUrl(), mt, recursive);
+                parse(asm.getUrl(), mt); // Recursive call
             }
-        } else if (!sm.isIndex()) {
+        } else {
             Collection<SiteMapURL> links = ((SiteMap) sm).getSiteMapUrls();
             for (SiteMapURL smu : links) {
                 System.out.println(smu.getUrl());
             }
         }
     }
-
-    public static void main(String[] args) throws IOException, UnknownFormatException {
-        if (args.length < 1) {
-            System.err.println("SiteMapTester URL_to_test [MimeType]");
-        }
-
-        URL url = new URL(args[0]);
-
-        String mt = null;
-
-        if (args.length > 1)
-            mt = args[1];
-
-        SiteMapTester tester = new SiteMapTester();
-        tester.parse(url, mt, true);
-    }
-
 }
