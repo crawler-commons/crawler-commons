960b7a8fed5cfcd8bfc82bb8628e269bfd7449a1
Sebastian Nagel <snagel@apache.org>
2017-02-02 15:18:05 +0100

    remove unused import

--- src/test/java/crawlercommons/sitemaps/SiteMapParserTest.java
+++ src/test/java/crawlercommons/sitemaps/SiteMapParserTest.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
