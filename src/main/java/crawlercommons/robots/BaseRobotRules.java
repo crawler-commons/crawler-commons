bb7523d9786d7ccadb303626e266f8d5b5fe8a5b
Avi Hayun <avraham2@gmail.com>
2019-09-11 18:22:41 +0300

    Updated the toString method to include a list of sitemaps if there are less than 10 sitemaps in the robots file

--- src/main/java/crawlercommons/robots/BaseRobotRules.java
+++ src/main/java/crawlercommons/robots/BaseRobotRules.java
@@ -24,7 +24,6 @@ import java.util.List;
  * Result from parsing a single robots.txt file - which means we get a set of
  * rules, and a crawl-delay.
  */
-
 @SuppressWarnings("serial")
 public abstract class BaseRobotRules implements Serializable {
 
@@ -68,11 +67,6 @@ public abstract class BaseRobotRules implements Serializable {
         return _sitemaps;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#hashCode()
-     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -83,11 +77,6 @@ public abstract class BaseRobotRules implements Serializable {
         return result;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -109,10 +98,9 @@ public abstract class BaseRobotRules implements Serializable {
         return true;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
+    /**
+     * Returns a string with the crawl delay as well as a list of sitemaps if
+     * they exist (and aren't more than 10)
      */
     @Override
     public String toString() {
@@ -124,14 +112,17 @@ public abstract class BaseRobotRules implements Serializable {
         } else {
             sb.append(" - crawl delay: ").append(delay).append('\n');
         }
+
         int nSitemaps = getSitemaps().size();
         if (nSitemaps == 0) {
             sb.append(" - no sitemap URLs\n");
         } else {
             sb.append(" - number of sitemap URLs: ").append(nSitemaps).append('\n');
-            sb.append("   sitemaps[0]: ").append(getSitemaps().get(0)).append('\n');
+            if (nSitemaps <= 10) {
+                sb.append(String.join("\n", getSitemaps())).append("\n\n");
+            }
         }
+
         return sb.toString();
     }
-
 }
