a19f9d55c769acee17b3afeeb90962fbc1201cf5
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2026-01-26 16:17:15 +0100

    Port IntDirectiveParser

---
+++ src/test/java/crawlercommons/robots_tag/parsers/IntDirectiveParserTest.java
@@ -0,0 +1,26 @@
+package crawlercommons.robots_tag.parsers;
+
+import crawlercommons.robots_tag.DirectiveParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("IntDirectiveParser")
+class IntDirectiveParserTest implements KeyValueDirectiveParserTest<Integer> {
+    @Override
+    public DirectiveParser<Integer> provideDirectiveParser() {
+        return IntDirectiveParser.getSingleton();
+    }
+
+    @Override
+    public Stream<Arguments> provideTestArguments() {
+        return Stream.of(
+            arguments("max-snippet", "123", 123),
+            arguments("max-snippet", "+123", 123),
+            arguments("max-snippet", "-123", -123)
+        );
+    }
+}
